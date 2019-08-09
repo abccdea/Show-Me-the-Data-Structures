affefc5bcdc1ce957af0c82baeda78688abf9c88
Zuowen Wang <mail@wangzuowen.me>
2019-08-09 22:27:28 +0800

    fix

--- problem_1.py
+++ problem_1.py
@@ -2,54 +2,33 @@ from collections import deque
 
 class LRU_Cache(object):
 
-    def __init__(self, capacity=5):
-        self.capacity = capacity
-        self.cache_dict = {}
-        self.cache_deque = deque()
-        
+    def __init__(self, capacity):
+       # Initialize class variables
+       self.cache_cap = capacity
+       self.cache_val = {}
+       self.cache_order = deque()
+
     def get(self, key):
-        if(self.cache_dict.get(key) is None):
+        # Retrieve item from provided key. Return -1 if non existant.
+        if key is None:
             return -1
-        else:            
-            self.cache_deque.remove(key)
-            self.cache_deque.appendleft(key)
-            return self.cache_dict.get(key)
-        
-	def set(self, key, value):
-		# Set the value if the key is not present in the cache. If the cache is at capacity remove the oldest item.
-		if len(self.cache_order) >= self.cache_cap:
-			del self.cache_val[self.cache_order.popleft()]
-		self.cache_order.append(key)
-		self.cache_val[key] = value
-            
-            
-            
-our_cache = LRU_Cache()
+
+        return self.cache_val.get(key, -1)
+
+    def set(self, key, value):
+        # Set the value if the key is not present in the cache. If the cache is at capacity remove the oldest item.
+        if len(self.cache_order) >= self.cache_cap:
+            del self.cache_val[self.cache_order.popleft()]
+        self.cache_order.append(key)
+        self.cache_val[key] = value
+'''
+For testing'''
+our_cache = LRU_Cache(2)
 
 our_cache.set(1, 1)
-
 our_cache.set(2, 2)
-our_cache.set(3, 3)
-our_cache.set(4, 4)
-
-
-print(our_cache.get(1))       # returns 1
-print(our_cache.get(2))      # returns 2
-print(our_cache.get(9))     # returns -1 because 9 is not present in the cache
-
-our_cache.set(5, 5)
-our_cache.set(1, 1)
-our_cache.set(6, 6)
-
-print(our_cache.get(3))      # returns -1 because the cache reached it's capacity and 3 was the least recently used entry
-
+our_cache.set(3, 2)
+print(our_cache.cache_val)
+print(our_cache.get(1))       # returns -1
 print(our_cache.get(2))       # returns 2
-our_cache.set(7, 7)
-our_cache.set(8, 8)
-print(our_cache.get(1))       # returns 1 since it was overwritten
-print(our_cache.get(6))       # returns 6
-print(our_cache.get(5))       # returns -1 since it was least used
-print(our_cache.get(0))       # returns -1 since it is non existent
-
-empty_cache = LRU_Cache(0)
-empty_cache.set(1, 1)	#prints Cannot add. Cache has empty capacity since it has 0 capacity
+print(our_cache.get(None))    # return -1
